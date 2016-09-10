baea60b2f68479468a6027e2c3dd9444a6de4e11
azgardTech <khreibi.mohamed@gmail.com>
2016-09-10 16:04:44 +0200

    add app.js file and make it the entry point of the app

--- Server.js
+++ Server.js
@@ -1,17 +1,17 @@
 var http= require('http');
 
-function onRequest(request,response)
+
+function StartServer()
 {
-			console.log("request recived");
-			response.writeHead(200,{"content-Type":"text/plain"});
-			response.write("My Node App");
-			response.end();
+		function onRequest(request,response)
+			{
+				console.log("request recived");
+				response.writeHead(200,{"content-Type":"text/plain"});
+				response.write("My Node App");
+				response.end();
+			}
+		http.createServer(onRequest).listen(8888);
+		console.log("sever on 8888");
+	}
 
-}
-
-
-http.createServer(onRequest).listen(8888);
-
-
-
-console.log("sever on 8888");
+exports.StartServer=StartServer;

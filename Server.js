e9870c712263201ced27b7784eb572cd73a09ba6
azgardTech <khreibi.mohamed@gmail.com>
2016-09-10 15:50:38 +0200

    Server implementation

---
+++ Server.js
@@ -0,0 +1,17 @@
+var http= require('http');
+
+function onRequest(request,response)
+{
+			console.log("request recived");
+			response.writeHead(200,{"content-Type":"text/plain"});
+			response.write("My Node App");
+			response.end();
+
+}
+
+
+http.createServer(onRequest).listen(8888);
+
+
+
+console.log("sever on 8888");
